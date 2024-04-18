b34456bebe6fe1dc82ec4e34786250db94cfc351
Patrick James Pascual <ppascual@slac.stanford.edu>
2024-04-18 13:14:44 -0700

    bugfix: Changed back to use echo with -en option and newline characters in redirect to xfce panel config XML.

--- template/desktops/xfce.sh
+++ template/desktops/xfce.sh
@@ -8,111 +8,110 @@ fi
 PANEL_CONFIG="${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
 if [[ ! -e "${PANEL_CONFIG}" ]]; then
   mkdir -p "$(dirname "${PANEL_CONFIG}")"
-  cat >"${PANEL_CONFIG}" <EOF
-<?xml version="1.0" encoding="UTF-8"?>
-
-<channel name="xfce4-panel" version="1.0">
-  <property name="configver" type="int" value="2"/>
-  <property name="panels" type="array">
-    <value type="int" value="1"/>
-    <value type="int" value="2"/>
-    <property name="dark-mode" type="bool" value="true"/>
-    <property name="panel-1" type="empty">
-      <property name="position" type="string" value="p=6;x=0;y=0"/>
-      <property name="length" type="uint" value="100"/>
-      <property name="position-locked" type="bool" value="true"/>
-      <property name="icon-size" type="uint" value="16"/>
-      <property name="size" type="uint" value="26"/>
-      <property name="plugin-ids" type="array">
-        <value type="int" value="1"/>
-        <value type="int" value="2"/>
-        <value type="int" value="3"/>
-        <value type="int" value="4"/>
-        <value type="int" value="5"/>
-        <value type="int" value="6"/>
-        <value type="int" value="7"/>
-        <value type="int" value="8"/>
-        <value type="int" value="9"/>
-        <value type="int" value="10"/>
-        <value type="int" value="11"/>
-        <value type="int" value="12"/>
-        <value type="int" value="13"/>
-        <value type="int" value="14"/>
-      </property>
-    </property>
-    <property name="panel-2" type="empty">
-      <property name="autohide-behavior" type="uint" value="1"/>
-      <property name="position" type="string" value="p=10;x=0;y=0"/>
-      <property name="length" type="uint" value="1"/>
-      <property name="position-locked" type="bool" value="true"/>
-      <property name="size" type="uint" value="48"/>
-      <property name="plugin-ids" type="array">
-        <value type="int" value="15"/>
-        <value type="int" value="16"/>
-        <value type="int" value="17"/>
-        <value type="int" value="18"/>
-        <value type="int" value="19"/>
-        <value type="int" value="20"/>
-        <value type="int" value="21"/>
-        <value type="int" value="22"/>
-      </property>
-    </property>
-  </property>
-  <property name="plugins" type="empty">
-    <property name="plugin-1" type="string" value="applicationsmenu"/>
-    <property name="plugin-2" type="string" value="tasklist">
-      <property name="grouping" type="uint" value="1"/>
-    </property>
-    <property name="plugin-3" type="string" value="separator">
-      <property name="expand" type="bool" value="true"/>
-      <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-4" type="string" value="pager"/>
-    <property name="plugin-5" type="string" value="separator">
-        <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-6" type="string" value="systray">
-        <property name="square-icons" type="bool" value="true"/>
-    </property>
-    <property name="plugin-8" type="string" value="pulseaudio">
-      <property name="enable-keyboard-shortcuts" type="bool" value="true"/>
-      <property name="show-notifications" type="bool" value="true"/>
-    </property>
-    <property name="plugin-9" type="string" value="power-manager-plugin"/>
-    <property name="plugin-10" type="string" value="notification-plugin"/>
-    <property name="plugin-11" type="string" value="separator">
-      <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-12" type="string" value="clock"/>
-    <property name="plugin-13" type="string" value="separator">
-      <property name="style" type="uint" value="0"/>
-    </property>
-    <property name="plugin-14" type="string" value="actions"/>
-    <property name="plugin-15" type="string" value="showdesktop"/>
-    <property name="plugin-16" type="string" value="separator"/>
-    <property name="plugin-17" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="xfce4-terminal-emulator.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-18" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="xfce4-file-manager.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-19" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="xfce4-web-browser.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-20" type="string" value="launcher">
-      <property name="items" type="array">
-        <value type="string" value="xfce4-appfinder.desktop"/>
-      </property>
-    </property>
-    <property name="plugin-21" type="string" value="separator"/>
-    <property name="plugin-22" type="string" value="directorymenu"/>
-  </property>
+  echo -en >"${PANEL_CONFIG}" <EOF
+<?xml version="1.0" encoding="UTF-8"?>\n\n
+<channel name="xfce4-panel" version="1.0">\n
+  <property name="configver" type="int" value="2"/>\n
+  <property name="panels" type="array">\n
+    <value type="int" value="1"/>\n
+    <value type="int" value="2"/>\n
+    <property name="dark-mode" type="bool" value="true"/>\n
+    <property name="panel-1" type="empty">\n
+      <property name="position" type="string" value="p=6;x=0;y=0"/>\n
+      <property name="length" type="uint" value="100"/>\n
+      <property name="position-locked" type="bool" value="true"/>\n
+      <property name="icon-size" type="uint" value="16"/>\n
+      <property name="size" type="uint" value="26"/>\n
+      <property name="plugin-ids" type="array">\n
+        <value type="int" value="1"/>\n
+        <value type="int" value="2"/>\n
+        <value type="int" value="3"/>\n
+        <value type="int" value="4"/>\n
+        <value type="int" value="5"/>\n
+        <value type="int" value="6"/>\n
+        <value type="int" value="7"/>\n
+        <value type="int" value="8"/>\n
+        <value type="int" value="9"/>\n
+        <value type="int" value="10"/>\n
+        <value type="int" value="11"/>\n
+        <value type="int" value="12"/>\n
+        <value type="int" value="13"/>\n
+        <value type="int" value="14"/>\n
+      </property>\n
+    </property>\n
+    <property name="panel-2" type="empty">\n
+      <property name="autohide-behavior" type="uint" value="1"/>\n
+      <property name="position" type="string" value="p=10;x=0;y=0"/>\n
+      <property name="length" type="uint" value="1"/>\n
+      <property name="position-locked" type="bool" value="true"/>\n
+      <property name="size" type="uint" value="48"/>\n
+      <property name="plugin-ids" type="array">\n
+        <value type="int" value="15"/>\n
+        <value type="int" value="16"/>\n
+        <value type="int" value="17"/>\n
+        <value type="int" value="18"/>\n
+        <value type="int" value="19"/>\n
+        <value type="int" value="20"/>\n
+        <value type="int" value="21"/>\n
+        <value type="int" value="22"/>\n
+      </property>\n
+    </property>\n
+  </property>\n
+  <property name="plugins" type="empty">\n
+    <property name="plugin-1" type="string" value="applicationsmenu"/>\n
+    <property name="plugin-2" type="string" value="tasklist">\n
+      <property name="grouping" type="uint" value="1"/>\n
+    </property>\n
+    <property name="plugin-3" type="string" value="separator">\n
+      <property name="expand" type="bool" value="true"/>\n
+      <property name="style" type="uint" value="0"/>\n
+    </property>\n
+    <property name="plugin-4" type="string" value="pager"/>\n
+    <property name="plugin-5" type="string" value="separator">\n
+        <property name="style" type="uint" value="0"/>\n
+    </property>\n
+    <property name="plugin-6" type="string" value="systray">\n
+        <property name="square-icons" type="bool" value="true"/>\n
+    </property>\n
+    <property name="plugin-8" type="string" value="pulseaudio">\n
+      <property name="enable-keyboard-shortcuts" type="bool" value="true"/>\n
+      <property name="show-notifications" type="bool" value="true"/>\n
+    </property>\n
+    <property name="plugin-9" type="string" value="power-manager-plugin"/>\n
+    <property name="plugin-10" type="string" value="notification-plugin"/>\n
+    <property name="plugin-11" type="string" value="separator">\n
+      <property name="style" type="uint" value="0"/>\n
+    </property>\n
+    <property name="plugin-12" type="string" value="clock"/>\n
+    <property name="plugin-13" type="string" value="separator">\n
+      <property name="style" type="uint" value="0"/>\n
+    </property>\n
+    <property name="plugin-14" type="string" value="actions"/>\n
+    <property name="plugin-15" type="string" value="showdesktop"/>\n
+    <property name="plugin-16" type="string" value="separator"/>\n
+    <property name="plugin-17" type="string" value="launcher">\n
+      <property name="items" type="array">\n
+        <value type="string" value="xfce4-terminal-emulator.desktop"/>\n
+      </property>\n
+    </property>\n
+    <property name="plugin-18" type="string" value="launcher">\n
+      <property name="items" type="array">\n
+        <value type="string" value="xfce4-file-manager.desktop"/>\n
+      </property>\n
+    </property>\n
+    <property name="plugin-19" type="string" value="launcher">\n
+      <property name="items" type="array">\n
+        <value type="string" value="xfce4-web-browser.desktop"/>\n
+      </property>\n
+    </property>\n
+    <property name="plugin-20" type="string" value="launcher">\n
+      <property name="items" type="array">\n
+        <value type="string" value="xfce4-appfinder.desktop"/>\n
+      </property>\n
+    </property>\n
+    <property name="plugin-21" type="string" value="separator"/>\n
+    <property name="plugin-22" type="string" value="directorymenu"/>\n
+  </property>\n
 </channel>
 EOF
 fi
