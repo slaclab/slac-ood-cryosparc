f71cfe052fb2864c64eb9b09902660a7b82dc169
Patrick James Pascual <ppascual@slac.stanford.edu>
2026-08-21 10:08:51 -0700

    bugfix: Attempt #5 to fix d-bus.

--- template/desktops/xfce.sh
+++ template/desktops/xfce.sh
@@ -123,8 +123,8 @@ fi
 #fi
 export XDG_RUNTIME_DIR="/run/user/$UID"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-export $(dbus-launch)
-sudo systemctl daemon-reload
+#export $(dbus-launch)
+#systemctl --user daemon-reload
 
 # Disable startup services
 xfconf-query -c xfce4-session -p /startup/ssh-agent/enabled -n -t bool -s false
