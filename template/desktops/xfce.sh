d68e8703594477b7528cb2563abc05d014c77814
Patrick James Pascual <ppascual@slac.stanford.edu>
2026-08-24 09:45:29 -0700

    bugfix: Attempt #12 to fix d-bus. Starting xfce-session by using dbus-launch without exec.

--- template/desktops/xfce.sh
+++ template/desktops/xfce.sh
@@ -158,4 +158,4 @@ export XDG_DATA_HOME="${LSCRATCH}/Desktop"
 
 echo "XDG_DESKTOP_DIR=${XDG_DESKTOP_DIR}"
 # Start up xfce desktop (block until user logs out of desktop)
-exec /bin/dbus-launch --exit-with-x11 /usr/bin/xfce4-session
+dbus-launch --exit-with-x11 /usr/bin/xfce4-session
